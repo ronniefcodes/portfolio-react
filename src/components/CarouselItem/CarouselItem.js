c76397283fe155375f29bb67c92ce99343226003
Ronnie Cheung <ronniefcheung@gmail.com>
2018-10-11 16:58:36 -0400

    Added Carousel component

---
+++ src/components/CarouselItem/CarouselItem.js
@@ -0,0 +1,10 @@
+import React, { Component, } from 'react';
+import styledComponents from 'styled-components';
+
+const CarouselItem = styledComponents.div`
+  height: 100%;
+  width: 100%;
+  text-align: center;
+`;
+
+export default CarouselItem;
